d01a1f2940b786b8549ba6dd94e009e3acdd6c36
cineryam <125714025+cineryam@users.noreply.github.com>
2023-02-18 11:02:18 +0700

    Update login-script.js
    Adding comments in English

--- Aplikasi-Login-01-starter-study-case/assets/scripts/login-script.js
+++ Aplikasi-Login-01-starter-study-case/assets/scripts/login-script.js
@@ -5,28 +5,32 @@
  * Use showPopUp() function to display error pop up
  */
 
-/* Making variable to save email and password information */
+/* Making variable for every view elements */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* Making variable to save email and password information */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Making click action to button */
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
     
-    // TODO 1 : Getting user's email and password input from form.
+    // TODO 1 : Getting user's email and password input when the button is pressed.
     const email = inputEmailElement.value;
     const password = inputPasswordElement.value;
 
     /* TODO 2 : Making comparison Logic with condition:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
+       If the email variable is identic with expectedEmail and password is identic with expectedPassword, call goToHome() function.
+       If not, then call showPopUp() function.
      */
     if (email == expectedEmail && password == expectedPassword){
+      /* if the condition is right, the program will display home page */  
       goToHome();
     } else {
+      /* if the condition is not right, the program will display information that the input is wrong */  
       showPopup();
     }
 });
